52ec7245ebb8003690f394e6aaacfff8c27a1a3b
moling3650 <365024424@qq.com>
2016-12-02 22:11:52 +0800

    导入数据库框架

--- www/app/__init__.py
+++ www/app/__init__.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from config import config
 
-def create_app():
+def create_app(config_name='default'):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     from app.routes.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
