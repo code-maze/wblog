64148fca7f22c620f203c25ff1c0af39b116f0c4
moling3650 <365024424@qq.com>
2016-12-02 15:58:38 +0800

    建立基础模板

--- www/app/routes/main.py
+++ www/app/routes/main.py
@@ -7,4 +7,20 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
+
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@main.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
+@main.route('/admin')
+def admin():
+    return render_template('admin.html')
+
