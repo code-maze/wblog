f85a0d21ae0a767f8ba8b52387a4067b0d518b8c
二大爷 <793507471@qq.com>
2016-12-03 16:15:56 +0800

    导入回到顶部插件

---
+++ www/app/static/js/backTop.js
@@ -0,0 +1,21 @@
+if (window.jQuery === undefined) {
+	throw new Error('backTop 插件库依赖jQuery'); 
+}
+jQuery.BackTop = function() {
+	var $backTop = $('<div id="backTop">');
+    $("body").append($backTop);
+	$(window).scroll(function() {
+		if ($(window).scrollTop() >= $(window).height()) {
+			$backTop.fadeIn();
+		} else {
+            $backTop.fadeOut();
+        }
+	});
+	$backTop.click(function() { // 监听鼠标单击事件
+		$backTop.css('background-position','0px -344px');
+		$("body").animate({scrollTop: 0}, 1000);	
+		$backTop.animate({bottom: '80%'}, 1000).animate({opacity: 0}, 500, function(){
+			$backTop.removeAttr('style').stop(true);
+		});		
+	});
+}
