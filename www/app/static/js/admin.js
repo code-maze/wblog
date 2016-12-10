68ee8258e781d501933b779ae8aeed7ea1d93734
二大爷 <793507471@qq.com>
2016-12-10 16:35:41 +0800

    管理页头部路径导航组件

--- www/app/static/js/admin.js
+++ www/app/static/js/admin.js
@@ -1,6 +1,17 @@
 var admin = {
     lists: null,
     pagination: null,
+    types: {
+        users: {
+            ths:'<tr><th>用户名</th><th>邮箱</th><th>注册时间</th></tr>' ,
+            tds: ['name','email','regTime']
+        },
+        blogs: {
+            ths:'<tr><th>标题</th><th>作者</th><th>发布时间</th></tr>' ,
+            tds: ['title','author','pubTime']
+        }
+
+    },
     init: function(type, page, size) {
         this.type = type || 'users';
         this.page = page || 1;
@@ -9,13 +20,23 @@ var admin = {
             this.paintTable.call(this);
             this.paintPagination.call(this);
         });
-        $("#pagination").on('click','li',(e)=>{
+        $("#pagination").on('click','a',(e)=>{
             e.preventDefault();
             var $a = $(e.target);
             this.page = $a.html();
             this.getData(this.paintTable.bind(this));
             $a.parent().addClass('active').siblings('.active').removeClass('active');  
         });
+        $(".breadcrumb").on('click','a',(e)=>{
+            e.preventDefault();
+            var $a=$(e.target);
+            this.type = $a.html();
+            this.page = 1;
+            this.getData(() => {
+                this.paintTable.call(this);
+                this.paintPagination.call(this);
+            });
+        })
     },
     getData: function (callback) {
         $.ajax({
@@ -41,10 +62,12 @@ var admin = {
     },
     paintTable: function() {
         let $table = $('<table class="table table-striped table-hover">');
-        let str = '<tr><th>用户名</th><th>邮箱</th><th>注册时间</th></tr>';
+        let str = this.types[this.type].ths;
         for (let i = 0; i < this.lists.length; i++) {
             let item = this.lists[i];
-            str += `<tr><td>${item["name"]}</td><td>${item["email"]}</td><td>${item["regTime"].toDateTime()}</td></tr>`;           
+            let td=this.types[this.type].tds;
+            // debugger;
+            str += `<tr><td>${item[td[0]]}</td><td>${item[td[1]]}</td><td>${item[td[2]].toDateTime()}</td></tr>`;           
         }
         $table.html(str);
         $('.table-responsive').html($table);
