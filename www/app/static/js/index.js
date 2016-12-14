16d20d52a26c68b7a84c115c0560b7f381565a8a
moling3650 <365024424@qq.com>
2016-12-14 10:23:13 +0800

    内容

--- www/app/static/js/index.js
+++ www/app/static/js/index.js
@@ -2,20 +2,28 @@
 * @Author: anchen
 * @Date:   2016-12-12 20:20:46
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-12-12 21:04:46
+* @Last Modified time: 2016-12-14 10:08:55
 */
+
+
 
 'use strict';
 $.ajax({
     url: 'api/v1.0/blogs',
     success:function(object){
-       var html=object.blogs;
-       var str='';
-       for(var i=0;i<html.length;i++){
-            var title=html[i].title;
-            var content=html[i].content;
-            var author=html[i].author;
-            var pubTime=html[i].pubTime;
+       var html = object.blogs;
+       var str = '';
+       showdown.setFlavor('github');
+        var converter = new showdown.Converter();
+
+       for(var i = 0; i < html.length; i++){
+            var title = html[i].title;
+            var content = converter.makeHtml(html[i].content);
+            var author = html[i].author;
+            var pubTime = html[i].pubTime;
+
+            console.log(content)
+
             
             str+=`<article>
                      <div class='box'>
@@ -33,6 +41,5 @@ $.ajax({
        }
        $('#main').html(str);
     }
-
-
 });
+
