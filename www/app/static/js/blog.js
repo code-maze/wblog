258596085234fab3033e811cd3acdccc600af13d
moling3650 <365024424@qq.com>
2016-12-15 14:15:35 +0800

    修改blog的布局

--- www/app/static/js/blog.js
+++ www/app/static/js/blog.js
@@ -13,18 +13,18 @@ var blog_view = {
         showdown.setFlavor('github');
         var converter = new showdown.Converter();
         var str = `        
-        <div id="hair">
+        <div class="author-info">
             <span class="auttor">作者</span>
-            <a href="">${blog.author}</a>
+            <a href="#">${blog.author}</a>
             <p>${blog.pubTime.toDateTime('yyyy.MM.dd')}</p>
             <span class="btn btn-small btn-success">添加关注</span>
         </div>
         <div id="main">
             <h2>${blog.title}</h2>
             <p>${converter.makeHtml(blog.content)}</p>
-            <span>&copy;&nbsp;&nbsp;&nbsp;著作权归作者所有</span>
+            <span class="copy pull-right">&copy; 著作权归作者所有</span>
         </div>`;
-        $('#total').html(str);
+        $('.article').html(str);
         $('pre code').each(function(i, block) {
             hljs.highlightBlock(block);
         });
