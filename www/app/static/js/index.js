3360f841be9c0e1f9d49015b1793e553b6f9ae0b
peixiaofei <492435376@qq.com>
2016-12-15 12:16:30 +0800

    picture
    lin_fan

--- www/app/static/js/index.js
+++ www/app/static/js/index.js
@@ -17,7 +17,12 @@ $.ajax({
             var pubTime2 = pubTime.toDateTime('hh:mm');
             str += `
             <li>
-               <time class="tmtime"><span>${author}</span><img src='/static/img/flower.jpg'><span>${pubTime1}</span><span>${pubTime2}</span>  </time>
+               <time class="tmtime">
+                   <span>${author}</span>
+                   <img src='/static/img/flowers.jpg' style='width:150px;height:100px;'>
+                   <span>${pubTime1}</span>
+                   <span>${pubTime2}</span> 
+                </time>
                 <div class="tmlabel">
                     <h2>${title}</h2>
                     <p>${content}</p>
